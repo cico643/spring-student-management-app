657abb639605fe7ca49d0f44eb85ca336e7bcd55
Cihat Yeşildağ <cico643@hotmail.com.tr>
2023-07-29 02:37:58 +0300

    Initial Commit

---
+++ src/test/java/com/cico643/studentmanagement/StudentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cico643.studentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
