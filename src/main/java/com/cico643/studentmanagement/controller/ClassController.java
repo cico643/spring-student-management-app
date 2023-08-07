e1419d27fa5601f9647edf71ef08eb37d19d1eba
Cihat Yeşildağ <cico643@hotmail.com.tr>
2023-08-08 00:57:41 +0300

    feat(Class): created getClassById endpoint

--- src/main/java/com/cico643/studentmanagement/controller/ClassController.java
+++ src/main/java/com/cico643/studentmanagement/controller/ClassController.java
@@ -2,12 +2,16 @@ package com.cico643.studentmanagement.controller;
 
 import com.cico643.studentmanagement.dto.CreateClassRequest;
 import com.cico643.studentmanagement.dto.GenericApiResponse;
+import com.cico643.studentmanagement.model.Class;
 import com.cico643.studentmanagement.service.ClassService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/class")
@@ -19,4 +23,11 @@ public class ClassController {
     public ResponseEntity<GenericApiResponse> create(@Valid @RequestBody CreateClassRequest request)  {
         return new ResponseEntity<>(classService.create(request), HttpStatus.CREATED);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GenericApiResponse<Class>> getClassById(@PathVariable int id, HttpServletResponse response) throws IOException {
+        return new ResponseEntity<>(classService.getClassById(id, response), HttpStatus.OK);
+    }
+
+
 }
--- src/main/java/com/cico643/studentmanagement/service/ClassService.java
+++ src/main/java/com/cico643/studentmanagement/service/ClassService.java
@@ -2,8 +2,13 @@ package com.cico643.studentmanagement.service;
 
 import com.cico643.studentmanagement.dto.CreateClassRequest;
 import com.cico643.studentmanagement.dto.GenericApiResponse;
+import com.cico643.studentmanagement.exception.ApiError;
+import com.cico643.studentmanagement.exception.KlassNotFoundException;
 import com.cico643.studentmanagement.model.Class;
+import com.cico643.studentmanagement.model.enumTypes.Role;
 import com.cico643.studentmanagement.repository.ClassRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +17,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.Map;
 
 @Service
@@ -39,5 +45,30 @@ public class ClassService {
                 .message("The class named: [" + _class.getTitle() +"] has been successfully created.")
                 .data(Map.of("id", _class.getId(), "title", _class.getTitle()))
                 .build();
+    }
+
+    public GenericApiResponse<Class> getClassById(Integer id, HttpServletResponse response) throws IOException {
+        var _class = this.classRepository.findById(id)
+                        .orElseThrow(() -> new KlassNotFoundException("Class could not find by id: [" + id + "]"));
+        log.info("Fetched class by id: [" + id + "]");
+
+        var principal = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        var user = this.authService.getUserByEmail(principal.getUsername());
+        if(user.getRole() != Role.ADMIN) {
+            if(!_class.getInstructor().getId().equals(user.getId())) {
+                response.setStatus(HttpStatus.FORBIDDEN.value());
+                ApiError errorMessage = new ApiError(HttpStatus.FORBIDDEN, "Access Denied");
+                new ObjectMapper().writeValue(response.getOutputStream(), errorMessage);
+            }
+        }
+
+        return GenericApiResponse
+                .<Class>builder()
+                .success(true)
+                .status(HttpStatus.OK)
+                .message("Fetched class by id: [" + id + "]")
+                .data(_class)
+                .build();
+
     }
 }
